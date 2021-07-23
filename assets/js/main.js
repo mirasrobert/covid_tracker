c14a865b21a31976fdbeb00e4c7b7fa7f4fa8981
mirasrobert <mrmirasrobert@gmail.com>
2021-07-23 20:36:55 +0800

    Search Cases

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,57 @@
-const worldRecordAPI = "https://covid-api.mmediagroup.fr/v1";
-
-/*
-async function worldCovidRecord() {
-  const response = await fetch(worldRecordAPI, { mode: "no-cors" });
+async function worldCovidRecord(queryParam) {
+  const response = await fetch(
+    `https://covid-api.mmediagroup.fr/v1/cases?ab=${queryParam}`,
+    {
+      "Content-Type": "application/json",
+    }
+  );
 
   const data = await response.json();
 
   return data;
 }
 
+function filterCountries() {
+  let searchCountry = document.getElementById("searchCountry");
+  let searchBtn = document.getElementById("searchBtn");
 
-worldCovidRecord().then((data) => {
-  console.log(data);
-});*/
+  searchBtn.addEventListener("click", () => {
+    let search = searchCountry.value.toLowerCase();
 
-fetch(worldRecordAPI, {
-  method: "GET",
-  header: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "OPTIONS,GET",
-  },
-})
-  .then((res) => res.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+    // IF search box is not empty
+    if (search != "") {
+      // Call to the API
+      worldCovidRecord(search).then((data) => {
+        // Do something on the data
+
+        let result = data.All;
+
+        // Append Data to Modal
+        document.getElementById(
+          "modal-title"
+        ).innerHTML = `<h5>${result.country}</h5>`;
+
+        document.getElementById("modal-body").innerHTML = `
+        <h5>
+          Confirmed Cases: ${result.confirmed}
+        </h5>
+        <h5>
+          Deaths: ${result.deaths}
+        </h5>
+        <h5>
+          Recovered: ${result.recovered}
+        </h5>
+
+        <small>Updated: ${result.updated}</small>`;
+
+        searchCountry.value = ""; // Clear the search input
+
+        $("#myModal").modal("show"); // Open Modal
+      });
+    } else {
+      console.log("No Input");
+    }
+  });
+}
+
+filterCountries();
