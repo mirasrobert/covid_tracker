fdcf8832d94f69f918daac93b612797c83a2b35c
mirasrobert <mrmirasrobert@gmail.com>
2021-07-27 22:39:18 +0800

    datatables

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,24 @@
+const baseUrl = "https://covid-api.mmediagroup.fr/v1";
+const baseUrl2 = "https://api.covid19api.com/summary";
+
 /*
  Return country cases
 */
 async function countryCovidCases(queryParam) {
-  const response = await fetch(
-    `https://covid-api.mmediagroup.fr/v1/cases?ab=${queryParam}`,
-    {
-      "Content-Type": "application/json",
-    }
-  );
+  const response = await fetch(`${baseUrl}/cases?ab=${queryParam}`, {
+    "Content-Type": "application/json",
+  });
+
+  const data = await response.json();
+
+  return data;
+}
+
+/* Returns Vaccinated */
+async function vaccinated() {
+  const response = await fetch(`${baseUrl}/vaccines?country=Global`, {
+    "Content-Type": "application/json",
+  });
 
   const data = await response.json();
 
@@ -23,7 +34,6 @@ function searchCountryCases() {
 
     // IF search box is not empty
     if (search != "") {
-
       // validation
       if (
         searchCountry.classList.contains("is-invalid") ||
@@ -62,24 +72,36 @@ function searchCountryCases() {
         searchCountry.value = ""; // Clear the search input
 
         $("#myModal").modal("show"); // Open Modal
-        
       });
     } else {
-
       console.log("No Input");
       searchCountry.classList.add("is-invalid");
       searchCountry.classList.add("border");
       searchCountry.classList.add("border-danger");
-
     }
   });
 }
 
-searchCountryCases();
+function numberFormat(n) {
+  let num = String(n).replace(/(.)(?=(\d{3})+$)/g, "$1,");
+
+  return num;
+}
+
+function appendDataUsingAjaxDataTable() {
+  $("#tbl-posts").DataTable({
+    ajax: {
+      url: "data.json", // JSON Data
+      dataSrc: "",
+    },
+
+    columns: [{ data: "id" }, { data: "title" }, { data: "body" }],
+  });
+}
 
 /* Return World Summary */
 async function global() {
-  const response = await fetch(`https://api.covid19api.com/summary`, {
+  const response = await fetch(`${baseUrl2}`, {
     "Content-Type": "application/json",
   });
 
@@ -88,8 +110,118 @@ async function global() {
   return data;
 }
 
-global().then((data) => {
-  document.getElementById(
-    "total-cases"
-  ).innerHTML = `<span>${data.Global.TotalConfirmed}</span>`;
+$(function () {
+  let deaths = 0;
+  let recovered = 0;
+
+  global().then((data) => {
+    // Dashboard
+
+    document.getElementById("total-cases").innerHTML = `<span>${numberFormat(
+      data.Global.TotalConfirmed
+    )}</span>`;
+
+    /*
+    document.getElementById("population").innerHTML = `<span>${numberFormat(
+      data.Global.population
+    )}</span>`;*/
+
+    document.getElementById("confirm-cases").innerHTML = `<span>${numberFormat(
+      data.Global.TotalConfirmed
+    )}</span>
+    <span><small class="new text-warning">+${
+      data.Global.NewConfirmed
+    }</small></span>
+    `;
+
+    document.getElementById("recovered").innerHTML = `<span>${numberFormat(
+      data.Global.TotalRecovered
+    )}</span>
+    <span><small class="new text-success">+${
+      data.Global.TotalRecovered
+    }</small></span>
+    `;
+
+    recovered = data.Global.TotalRecovered;
+
+    document.getElementById("deaths").innerHTML = `<span>${numberFormat(
+      data.Global.TotalDeaths
+    )}</span><span><small class="new text-danger">+${
+      data.Global.TotalDeaths
+    }</small></span>
+    `;
+
+    deaths = data.Global.TotalDeaths;
+
+    searchCountryCases(); // search countries
+
+    // Datatables
+    let countries = data.Countries; // Array of countries
+
+    let tbody = "";
+
+    countries.forEach((country) => {
+      // Populate the table with data from API
+
+      tbody += `
+      <tr>  
+        <td> 
+        <span><img src="https://www.countryflags.io/${
+          country.CountryCode
+        }/flat/16.png">
+        </span> ${country.Country}
+        </td>
+        <td>
+        ${numberFormat(country.TotalConfirmed)}
+        <span class="text-warning">+${country.NewConfirmed}</span>
+        </td>
+        <td>
+        ${numberFormat(country.TotalRecovered)}
+        <span class="text-success">+${country.NewRecovered}</span>
+        </td>
+        <td>
+        ${numberFormat(country.TotalDeaths)}
+        <span class="text-danger">+${country.NewDeaths}</span>
+        </td>
+      </tr>
+      `;
+    });
+
+    document.getElementById("tbody").innerHTML = tbody; // append the tbody data
+
+    // Datatables
+    $("#example").DataTable();
+  });
+
+  vaccinated()
+    .then((response) => {
+      const vaccinated = response.All.people_vaccinated;
+
+      const data = {
+        labels: ["Deaths", "Recovered", "Vaccinated"],
+        datasets: [
+          {
+            label: "",
+            data: [deaths, recovered, vaccinated],
+            backgroundColor: [
+              "rgb(255, 99, 132)",
+              "rgb(54, 162, 235)",
+              "rgb(255, 205, 86)",
+            ],
+            hoverOffset: 4,
+          },
+        ],
+      };
+
+      const config = {
+        type: "doughnut",
+        data: data,
+      };
+
+      var myChart = new Chart(
+        document.getElementById("global-cases-chart"),
+        config
+      );
+    })
+    .catch((err) => console.error(err));
 });
